bd3917ce6ee3a7d92912d8415faf6b985c189e7f
MicaelPereira <micael.pereira@icarotech.com>
2025-06-30 16:30:36 -0300

    feat: ajuste das controllers para conexão do front react

--- src/main/java/com/example/sistema_vendas/controller/ClienteController.java
+++ src/main/java/com/example/sistema_vendas/controller/ClienteController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.*;
 
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequestMapping("/clientes")
 public class ClienteController {
